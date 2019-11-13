a88fe5225d367c2ee083e570d43f4db35014b31e
Heinz Araque <araqueheinz@gmail.com>
2019-11-13 08:06:56 -0500

    View element, Props, Styles, exercises

--- AppSetup/src/screens/ComponentsScreen.js
+++ AppSetup/src/screens/ComponentsScreen.js
@@ -2,11 +2,34 @@
 import React from 'react';
 
 // Import Text adn Stylesheet components from react-native
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
+  const greeting = 'Hey, how you doing?';
+
+  const textElement = <Text>Variable containing Text element</Text>
+
+  const name = 'Heinz Araque';
+
+
   return (
-    <Text style={styles.textStyle}> This is the component screen</Text>
+    <View>
+
+      <Text style={styles.textStyle}>This is the component screen</Text>
+
+      <Text>Using View to group Text together </Text>
+
+      <Text>{greeting}</Text>
+
+      {textElement}
+
+      <Text>JSX Exercise:</Text>
+
+      <Text style={styles.exerciseStyle}> Getting started with React Native</Text>
+
+      <Text style={styles.nameStyle}>My name is {name}</Text>
+
+    </View>
   );
 };
 
@@ -14,6 +37,14 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
     color: '#00ffff',
+  },
+  exerciseStyle: {
+    fontSize: 45,
+    color: 'blue'
+  },
+  nameStyle: {
+    fontSize: 20,
+    color: 'orange'
   }
 });
 
